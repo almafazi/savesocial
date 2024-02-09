5d85df669860ed053065ae58d17bf7ecf6541cef
Almafazi <almafazi@icloud.com>
2024-02-09 20:44:17 +0700

    edit home

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('dashboard', 'App\Http\Controllers\Frontend\FrontendController@index'
 */
 Route::group(['namespace' => 'App\Http\Controllers\Frontend', 'as' => 'frontend.'], function () {
     Route::get('/', function() {
-        return view('home');
+        return view('home-home-tiktok');
     })->name('index');
     Route::get('/tiktok', function() {
         return view('home-tiktok');
