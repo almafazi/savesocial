4ce9fde4f317270d389106f1708760e7e5e8207d
Almafazi <almafazi@icloud.com>
2024-03-07 15:24:49 +0700

    test

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -49,6 +49,7 @@ class YoutubeController extends Controller
     public function convert_api(Request $request) {
         $id = $request->id;
         if (Storage::disk('public')->exists('mp3/'.$id.'/*.mp3')) {
+            dd('dd');
             foreach (Storage::disk('public')->allFiles('mp3/'.$id) as $file) {
                 if (pathinfo($file, PATHINFO_EXTENSION) == 'mp3') {
                     $pathinfo = pathinfo($file);
